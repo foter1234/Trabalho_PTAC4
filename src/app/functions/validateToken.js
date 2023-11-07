a3cebd0b492c387bb3577d3740cd5af8748ddad0
foter1234 <lucaspesteves2017@gmail.com>
2023-11-07 17:27:03 -0400

    atualização

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -4,12 +4,17 @@ const validateToken = async (token)=>{
     
     const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTY5MTU0MiwiaWF0IjoxNjk1NjkxNTQyfQ.EG-A9n08TjSSm2affCX5Bl3yRpDbIytMIO5j2iMalLA"
     try {
-        const isTokenValidate =  await jwtVerify(token,  new TextEncoder().encode(secret));
+      /*  const isTokenValidate =  await jwtVerify(token,  new TextEncoder().encode(secret));
 
         if (isTokenValidate) {
             return true
-        }
+        }*/
 
+        const isTokenValidate = await decode(token)
+        if(isTokenValidate){
+            return true
+
+        }
     } catch {
         return false
     }
